0361c1a89297c2e73d50fcc2152d00e002442ee4
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2008-05-11 14:27:11 -0400

    It actually strips HTML now.

--- plugins/modifier.markdown.php
+++ plugins/modifier.markdown.php
@@ -2,6 +2,7 @@
 function smarty_modifier_markdown($text) {
     $parser = new Markdown_Parser;
     $parser->no_markup = true;
+    $parser->no_entities = true;
     $html = $parser->transform($text);
 
     return $html;
